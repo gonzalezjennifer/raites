2a2b60f7c4f6e5609a8a8f26c6da3e30e52464e8
Jose Emmanuel Rangel Rodriguez <je.rangelrodriguez@ugto.mx>
2023-04-26 18:40:23 -0600

    insertar usuarios

--- back/insertarusuario.php
+++ back/insertarusuario.php
@@ -3,19 +3,21 @@
     $conexion = conectar();
     $nombre = $_POST['nombre'];
     $apaterno = $_POST['apaterno'];
+    $amaterno = $_POST['amaterno'];
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
     $numero = $_POST['numero'];
     $tipousuario = $_POST['tipousuario'];
 
-    $query = "INSERT INTO usuario (nombre, apaterno, correo, contrasena, fotoperfil, numero, tipousuario) 
-                VALUES('$nombre', '$apaterno', '$correo', '$contrasena', '', '$numero', '$tipousuario')";
+    $query = "INSERT INTO usuario (nombre, apaterno, amaterno, correo, contrasena, fotoperfil, numero, tipousuario) 
+                VALUES('$nombre', '$apaterno', '$amaterno', '$correo', '$contrasena', '', '$numero', '$tipousuario')";
 
     $resultado = mysqli_query($conexion, $query);
 
     if($resultado) {
-        header('location: ../front/index.php');
+        header('location: ../front/index.html');
     } else {
+        echo $resultado;
         echo 'FUCK ';
     }
 ?>
