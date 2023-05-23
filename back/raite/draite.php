7ce2b53c90583985dd6e71eb01f9e9a0ab12b9d9
FUANITO2029 <juanito2029@live.com.mx>
2023-05-22 18:54:07 -0600

    Se agregó el modal para el registro de raites

--- back/raite/draite.php
+++ back/raite/draite.php
@@ -1,25 +1,25 @@
 <?php
     session_start();
-    if(!empty($_POST["btnregistro"])) {
-        if (empty($_POST["origen"]) || empty($_POST["destino"]) || empty($_POST["hora"]) || empty($_POST["fotoauto"]) || empty($_POST["lugares"]) ){
-            echo '<div class="alert alert-danger">CAMPO/S VACIO/S</div>';
+    include_once '../conexion.php';
+    if (empty($_POST["origen"]) || empty($_POST["destino"]) || empty($_POST["hora"]) || empty($_POST["lugares"]) ){
+        echo '<div class="alert alert-danger">CAMPO/S VACIO/S</div>';
+    } else {
+        $conexion = conectar();
+        $idraitero = $_SESSION["id"];
+        $origen = $_POST['origen'];
+        $destino = $_POST['destino'];
+        $hora = $_POST['hora'];
+        $pasapor = $_POST['pasapor'];
+        $lugares = $_POST['lugares'];
+        $query = "INSERT INTO raite (idraitero, origen, destino, hora, pasapor, lugares) VALUES('$idraitero', '$origen', '$destino', '$hora', '$pasapor', '$lugares')";
+        $resultado = mysqli_query($conexion, $query);
+        if($resultado) {
+            echo '<div class="alert alert-success">REGISTRO EXITOSO</div>';
+            header("location: ../../front/components/usuarioconductor");
         } else {
-            $conexion = conectar();
-            $idraitero = $_GET['user'];
-            $origen = $_POST['origen'];
-            $destino = $_POST['destino'];
-            $hora = $_POST['hora'];
-            $fotoauto = $_POST['fotoauto'];
-            $pasapor = $_POST['pasapor'];
-            $lugares = $_POST['lugares'];
-            $query = "INSERT INTO raite (idraitero, origen, destino, hora, fotoauto, pasapor, lugares) VALUES('$idraitero', '$origen', '$destino', '$hora', '$fotoauto', '$pasapor', '$lugares')";
-            $resultado = mysqli_query($conexion, $query);
-            if($resultado) {
-                echo '<div class="alert alert-success">REGISTRO EXITOSO</div>';
-            } else {
-                echo '<div class="alert alert-danger">ERROR INESPERADO, INTENTE MAS TARDE</div>';
-            } 
-        }
-    } 
+            echo '<div class="alert alert-danger">ERROR INESPERADO, INTENTE MAS TARDE</div>';
+        } 
+    }
+
     
 ?>
--- front/components/usuarioconductor/index.php
+++ front/components/usuarioconductor/index.php
@@ -4,7 +4,8 @@
         header('location: ../login/login.php');
     }
 
-    $user = $_GET['user'];
+    $user = $_SESSION["id"];
+    // $user = $_GET['user'];
     echo "usuario conductor con nua:";
     echo $user;
 ?>
@@ -20,8 +21,47 @@
   <body>
 
     <a href="../../../back/login/control_logout.php" class="btn btn-danger">Salir</a>
-    <a href="../draite/raite.php?user=<?php echo $user ?>" class="btn btn-primary">Ofrecer raite</a>
+    <!-- <a href="../draite/raite.php?user=<?php echo $user ?>" class="btn btn-primary">Ofrecer raite</a> -->
 
+    <div class="Cabecera" style="display:flex; justify-content:space-around">
+        <p>Raites</p>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">+</button>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Registar nuevo raite</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+
+          <form action="../../../back/raite/draite.php" method="POST" class="form">
+            <input type="text" disabled="disabled" name="idRaitero" placeholder="<?php echo 'ID: ' . $user ?>" value="<?php $user ?>" class="form-control mb-3">
+            <input type="text" name="origen" placeholder="Origen" class="form-control mb-3">
+            <input type="text" name="destino" placeholder="Destino" class="form-control mb-3">
+            <input type="time" name="hora" placeholder="Hora salida" class="form-control mb-3">
+            <input type="text" name="pasapor" placeholder="Lugares por donde pasa" class="form-control mb-3">
+            <select id="cars" name="lugares" class="form-control mb-3">
+              <option value="none">Elige tus lugares disponibles</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+            
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Registar</button>
+            </div>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
