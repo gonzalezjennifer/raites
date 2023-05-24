7f993f4209fe8aadfb780f0af3b38ba660f6862e
Jose Emmanuel Rangel Rodriguez <je.rangelrodriguez@ugto.mx>
2023-05-23 21:23:48 -0600

    apartar raite

--- front/components/usuariopasajero/index.php
+++ front/components/usuariopasajero/index.php
@@ -22,7 +22,8 @@
   </head>
   <body>
 
-    <a href="../../../back/login/control_logout.php" class="btn btn-danger">Salir</a>  
+    <a href="../../../back/login/control_logout.php" class="btn btn-danger">Salir</a>
+    <a href="./verraites.php" class="btn btn-success">Apartar raite</a>  
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
