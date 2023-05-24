8f8af9461a67ae0e6ebc30a5a78c32a7d0122c24
marcdev22 <123595952+marcdev22@users.noreply.github.com>
2023-05-24 13:53:04 -0600

    Login 1.1
    Modificación del login con diseño responsive y nueva paleta de colores

--- front/components/login/login.php
+++ front/components/login/login.php
@@ -1,64 +1,92 @@
 <?php
-  include ("../../../back/conexion.php");
-  include ("../../../mcript.php");
+    include ("../../../back/conexion.php");
+    include ("../../../mcript.php");
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../style/login.css">
-    <title>Raites</title>
-  </head>
-  <body style="background-color: whitesmoke;">
-    <div class="principal">
-    <div class="barraTop">
-    <img src="../../img/bee.png" alt="Logo BeeRaites" class="logoBeeRaites">
-    <p class="nombrePagina"> BeeRaites</p>
-  </div>
-  <section class=" gradient-custom" style="height: 92vh">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center" style="height: 80%">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card text-white" style="border-radius: 1rem; background-color: #dab600;">
-          <div class="card-body p-5 text-center">
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"/>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+                <link href="../../styles/login.css" rel="stylesheet" />
+        
+        <title>Iniciar sesión</title>
+    </head>
+    <body class="" style="background-color: #242526;">
+        <header>
+            <!-- Navbar section -->
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top shadow">
+                <section class="container-md">
+                    <a class="navbar-brand mb-0 h1 fs-3">
+                        <img class = "d-inline-block align-text-center" src="./img/logo160x160.png" width="50" height="50"/>
+                        <span class = "bee-logo">
+                            <span class="text-warning">Bee</span>Raites <!--##ffe484-->
+                        </span>
+                    </a>
+                </section>
+           </nav>
+        </header>
+        <main class="container" >
+        <!--<main class="container mt-md-4 d-flex align-items-center justify-content-center w-100">-->
+            <section class = "row vh-100 align-items-center justify-content-center">
+                <div class = "login col-sm-8 col-md-6 col-lg-4 p-4" >
+                    <h2 class="text-center text-white mb-5">Inicia sesión en BeeRaites</h2>
+                    
+                    <?php 
+                        include("../../../back/login/control_login.php");
+                    ?>
 
-            <div class="mb-md-5 mt-md-4 pb-5">
-
-              <h2 class="fw-bold mb-5 text-uppercase">Login</h2>
-              
-              <?php 
-                include("../../../back/login/control_login.php");
-              ?>
-
-              <form action="" method="post">
-                <input type="email" name="correo" placeholder="Correo" class="form-control mb-3">
-                <input type="password" name="contrasena" placeholder="Ingresa la contraseña" class="form-control mb-3">
-                <div class="botones">
-                  <a href="../../index.html" class="btn btn-light" style="width: 40%; height: 10%;">Regresar</a>
-                  <input type="submit" name="btningresar" class="btn btn-dark mb-3" value="Iniciar sesion" style="width: 40%; height: 10%; margin-left: 10px;">
+                    <form action="" method="post">
+                        <div class="mb-4">
+                            <label class="form-label" for="email">Correo electrónico</label>
+                            <input class="form-control" name="email" type="email" placeholder="correo@ejemplo.com" required />
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label" for="password">Contraseña</label>
+                            <input class="form-control" name="password" type="password" required />
+                        </div>
+                        <div class="mb-4">
+                            <input class="btn btn-warning w-100 mt-4" type="submit" value="Iniciar"/>
+                        </div>
+                    </form>
+                    <p class="text-white text-center">
+                        ¿Aún no tienes cuenta?
+                        <a href="" style="text-decoration: none;">
+                            Regístrate aquí
+                        </a>
+                    </p>
                 </div>
-              </form>
-
-            </div>
-
-            <div>
-              <p class="mb-0">Don't have an account? <a href="../registro/insertarusuario.php" class="text-black-50 fw-bold">Sign Up</a>
-              </p>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
+            </section>
+<!--
+            <section class="login">
+                <form action = "" method = "post">
+                    <h2 class = "form-title text-center">Iniciar sesión</h2>
+                    <div class = "mb-2">
+                        <label class = "form-label" for="email">Correo electrónico</label>
+                        <input class="form-control" type = "email" placeholder="correo@ejemplo.com" required/> g
+                    </div>
+                    <div class = "mb-3">
+                        <label class="form-label" for="password">Contraseña</label>
+                        <input class = "form-control" type  ="password" required />
+                        <span class = "form-text">
+                            ¿Olvidaste tu contraseña? <a class = "" href="" style = "text-decoration: none;">Recuperar contraseña</a>
+                        </span>
+                    </div>
+                    <div class="mb-3">
+                        <input class = "btn btn-warning w-100 mt-2" type="submit" value="Ingresar"/>
+                    </div>
+                </form>
+                <span class="form-text mt-3  ">
+                    ¿Todavía no tienes una cuenta? <a class = "" href="" style = "text-decoration: none;">Regístrate aquí</a>
+                </span>
+            </section>
+-->
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>
 </html>
