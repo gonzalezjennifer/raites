6116eb7a38266533c78c7e62a5b43a17519fd26c
gonzalezjennifer <62447915+gonzalezjennifer@users.noreply.github.com>
2023-05-24 23:29:03 -0600

    tabla de raites pasajero

--- back/raite/verraites.js
+++ back/raite/verraites.js
@@ -1,3 +1,11 @@
+var DataTable = require( 'datatables.net' );
+require( 'datatables.net-responsive' );
+ 
+let table = new DataTable('#tablaraites', {
+    responsive: true
+});
+
+
 $(document).ready(function() {
     $('#tablaraites').DataTable({
         "aLengthMenu": [[5, 10, 15, -1], [5, 10, 15, "Todos"]],
